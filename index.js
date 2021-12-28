09651d5f4f387f4d4b91d6232248a8d715870633
siyamrh7 <siyamrh7@gmail.com>
2021-12-28 16:44:19 -0400

    soome

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const cors = require('cors')
 const mongoose =require('mongoose')
 const router=require('./routes/router')
 
-mongoose.connect('mongodb+srv://siyam:siyam123789654@cluster0.3xxfy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true })
-.then(res=>console.log("Database is connected")).catch(err=>console.log(err))
 
 const app=express()
+
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true ,tls:true,tlsCAFile:'./ca-certificate.crt'})
+.then(res=>console.log("Database is connected")).catch(err=>console.log(err))
 
 app.use(cors())
 app.use(express.json())
